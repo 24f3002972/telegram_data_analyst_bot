cef5aed67bce98c7d585acfff07f77b34c279778
Afna <24f3002972@ds.study.iitm.ac.in>
2026-07-30 11:09:50 +0530

    changes

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-import os
+from flask import Flask, send_file
+from threading import Thread
 from pathlib import Path
 
-from dotenv import load_dotenv
-from flask import Flask, request, send_file, abort
-
-from telegram import Update
-
 from bot import telegram_app
-
-load_dotenv()
 
 app = Flask(__name__)
 
@@ -23,26 +17,22 @@ def home():
 
 @app.get("/run.jsonl")
 def run_log():
-
     LOG_DIR.mkdir(exist_ok=True)
 
     if not LOG_FILE.exists():
         LOG_FILE.write_text("", encoding="utf-8")
 
-    return send_file(LOG_FILE)
+    return send_file(LOG_FILE, mimetype="application/json")
 
 
-@app.post("/webhook")
-async def webhook():
-
-    data = request.get_json(force=True)
-
-    update = Update.de_json(data, telegram_app.bot)
-
-    await telegram_app.process_update(update)
-
-    return "OK", 200
+def start_bot():
+    telegram_app.run_polling()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    Thread(target=start_bot, daemon=True).start()
+
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+    )
